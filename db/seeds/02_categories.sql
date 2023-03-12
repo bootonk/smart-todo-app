0610646b8b05b29fd47d174572b67aff4e290d64
Kate <115913025+bootonk@users.noreply.github.com>
2023-03-12 13:52:57 -0700

    Create tables with first seed data

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,43 @@
+-- Categories table seeds here
+-- Category options: READ, WATCH, BUY, EAT, NULL
+
+-- user_id = 1
+INSERT INTO categories (name, todo_id) VALUES ('READ', 1);
+INSERT INTO categories (name, todo_id) VALUES ('READ', 2);
+INSERT INTO categories (name, todo_id) VALUES ('READ', 3);
+INSERT INTO categories (name, todo_id) VALUES ('READ', 4);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 5);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 6);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 7);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 8);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 9);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 10);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 11);
+INSERT INTO categories (name, todo_id) VALUES (NULL, 12);
+
+-- user_id = 2
+INSERT INTO categories (name, todo_id) VALUES ('READ', 13);
+INSERT INTO categories (name, todo_id) VALUES ('READ', 14);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 15);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 16);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 17);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 18);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 19);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 20);
+INSERT INTO categories (name, todo_id) VALUES ('EAT', 21);
+INSERT INTO categories (name, todo_id) VALUES ('EAT', 22);
+INSERT INTO categories (name, todo_id) VALUES (NULL, 23);
+
+-- user_id = 3
+INSERT INTO categories (name, todo_id) VALUES ('READ', 24);
+INSERT INTO categories (name, todo_id) VALUES ('READ', 25);
+INSERT INTO categories (name, todo_id) VALUES ('READ', 26);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 27);
+INSERT INTO categories (name, todo_id) VALUES ('WATCH', 28);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 29);
+INSERT INTO categories (name, todo_id) VALUES ('BUY', 30);
+INSERT INTO categories (name, todo_id) VALUES ('EAT', 31);
+INSERT INTO categories (name, todo_id) VALUES ('EAT', 32);
+INSERT INTO categories (name, todo_id) VALUES ('EAT', 33);
+INSERT INTO categories (name, todo_id) VALUES ('EAT', 34);
+INSERT INTO categories (name, todo_id) VALUES (NULL, 35);
---
+++ db/seeds/03_todos.sql
@@ -0,0 +1,43 @@
+-- Todos table seeds here
+-- Category options: READ, WATCH, BUY, EAT, NULL
+
+-- user_id = 1
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 1, 'Love in the Time of Cholera', '2023-01-01 00:00:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 2, 'To Kill a Mockingbird', '2023-01-04 02:12:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 3, 'The Great Gatsby', '2023-01-07 04:24:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 4, '1984', '2023-01-10 06:37:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 5, 'The Godfather', '2023-01-13 08:49:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 6, 'Lord of the Rings', '2023-01-16 11:01:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 7, 'Forrest Gump', '2023-01-19 13:14:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 8, 'The Matrix', '2023-01-22 15:26:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 9, 'Silk Sleep Mask', '2023-01-25 17:38:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 10, 'Silk Slippers', '2023-01-28 19:51:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 11, 'Silk Robe', '2023-01-31 22:03:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (1, 12, 'Alphabet Soup', '2023-02-04 00:15:40', FALSE);
+
+-- user_id = 2
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 13, 'The DaVinci Code', '2023-02-07 02:28:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 14, 'Pride and Prejudice', '2023-02-10 04:40:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 15, 'Severance', '2023-02-13 06:52:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 16, 'Andor', '2023-02-16 09:05:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 17, 'Maury', '2023-02-19 11:17:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 18, 'Bug Spray', '2023-02-22 13:29:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 19, 'Mosquito Net', '2023-02-25 15:42:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 20, 'Anti-itch cream', '2023-02-28 17:54:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 21, 'Big Mac', '2023-03-03 20:06:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 22, 'Chocolate Pudding', '2023-03-06 22:19:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (2, 23, 'Time Magazine', '2023-03-10 00:31:20', FALSE);
+
+-- user_id = 3
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 24, 'Love in the Time of Cholera', '2023-03-13 02:43:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 25, 'Babar', '2023-03-16 04:56:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 26, 'Game of Thrones', '2023-03-19 07:08:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 27, 'Sex and the City', '2023-03-22 09:20:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 28, 'Hamilton', '2023-03-25 11:33:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 29, 'Umbrella', '2023-03-28 13:45:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 30, 'Rainboots', '2023-03-31 15:57:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 31, 'Lasagne', '2023-04-03 18:10:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 32, 'Seven Layer Dip', '2023-04-06 20:22:20', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 33, 'Icecream Cake', '2023-04-09 22:34:40', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 34, 'Tortilla Chips', '2023-04-13 00:47:00', FALSE);
+INSERT INTO todos (user_id, category_id, name, date, done) VALUES (3, 35, 'Clothes Pins', '2023-04-16 02:59:20', FALSE);
