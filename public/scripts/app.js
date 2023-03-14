ff8d58a836172eb9c00ec3f0a9239aa6fb77da0b
sarah-zhan <yizh.contact@gmail.com>
2023-03-13 17:01:30 -0700

    add autocomplete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -58,6 +58,19 @@ $(function() {
 
     
     //auto complete
+    const databaseAutoComplete = [
+      'Love in the Time of Cholera',
+      'To Kill a Mockingbird',
+      'The Great Gatsby',
+      'The Godfather',
+      'Lord of the Rings',
+      'Forrest Gump',
+      'The Matrix',
+      'Silk Sleep Mask',
+      'Silk Slippers',
+      'Silk Robe',
+      'Alphabet Soup',
+    ];
     $("#inputText").autocomplete({
       source: databaseAutoComplete
     });
