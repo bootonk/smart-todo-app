b5bd759b40b3885e187555c5d0b0207f0a0c6f27
tessfbs <tessfbs@gmail.com>
2023-03-13 23:36:03 -0400

    start implement delete function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,7 @@
 // const databaseAutoComplete = require('autoComplete.js');
 
 
-$(function() {
+$(function () {
 
   console.log("app.js running");
 
@@ -15,28 +15,25 @@ $(function() {
   <li>
     <input id="checkbox-1" type="checkbox">
     <label for="checkbox-1">${todo.name}<span class="box"></span></label>
+    <button type="submit" class="btn btn-warning btn-sm" >Edit</button>
+    <button type="submit" class="btn btn-danger btn-sm delete" id="${todo.id}">Delete</button>
   </li>
 `)
-  return $todo;
-}
-
-  // //render Todos
-  // const renderTodos = (database) => {
-  //   $("#tab-1").empty();
-  //     database.forEach(todo => {
-  //       $("#tab-1").append(createTodoElement(todo))
-  //     });
-  // }
+    return $todo;
+  }
 
   //load Todos by category
   const loadTodos = () => {
 
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 4; i++) {
       $.get(`api/lists/${i}`, (todos) => {
         // renderTodos(todos);
         $(`#tab-${i}`).empty();
         todos.forEach(todo => {
           $(`#tab-${i}`).append(createTodoElement(todo));
+          $(`#${todo.id}`).click(function () {
+            alert(`Alert for ${todo.id}.click() called.`);
+        });
         });
       });
 
@@ -45,7 +42,7 @@ $(function() {
 
   //add count to category tab
   const categoryCounter = () => {
-    for(let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 4; i++) {
       $.get(`api/lists/count/${i}`, (count) => {
         //update count
         $(`a[href='#tab-${i}'] .count`).text(`(${count})`);
@@ -53,37 +50,34 @@ $(function() {
     }
   };
 
-  // //post input field
-  // $('#form-input').on('submit', function(event) {
-  //   event.preventDefault();
-  //   let todo = $('#form-input').serialize();
-
-  //   $.post('/api/lists', todo, () => {
-  //     $('#inputText').val('');
-  //     loadTodos();
-  //   });
-  // });
-
-
-  $('#form-input').on('submit', function(event) {
+  // add new todo
+  $('#form-input').on('submit', function (event) {
     event.preventDefault();
     let todoName = $('#inputText').val();
     let category_id = 2;
 
-    $.post('/api/lists', { todo_name: todoName, category_id: category_id})
-    
-      .then ((data) => {
-      // add the new todo item to the DOM
-      let $todoItem = createTodoElement(data);
-      $(`#tab-${category_id}`).append($todoItem);
-      $('#inputText').val('');
-      categoryCounter();
-    })
+    $.post('/api/lists', { todo_name: todoName, category_id: category_id })
+
+      .then((data) => {
+        // add the new todo item to the DOM
+        let $todoItem = createTodoElement(data);
+        $(`#tab-${category_id}`).append($todoItem);
+        $('#inputText').val('');
+        categoryCounter();
+      })
   });
 
+  // delete new todo
 
-    loadTodos();
-    categoryCounter();
+  
+  
+  loadTodos();
+  $('.delete').click(function () {
+    alert("Handler for .click() called.");
+});
+  categoryCounter();
+  
+
 });
 
 
