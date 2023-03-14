99a434917e37caf336ac74ef490fe676e57d6a6e
sarah-zhan <yizh.contact@gmail.com>
2023-03-14 14:44:28 -0700

    update autocomplet and delete function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 // Client facing scripts here
+// const databaseAutoComplete = require('/db/autoComplete/autoComplete.js');
 
-
-$(function () {
+$(function() {
 
   console.log("app.js running");
 
@@ -11,15 +11,15 @@ $(function () {
   //create a new element for todo
   const createTodoElement = (todo) => {
     let $todo = $(`
-  <div>
+  <div class="${todo.id}">
     <input id="checkbox-1" type="checkbox">
     <label for="checkbox-1">${todo.name}<span class="box"></span></label>
     <button type="submit" class="btn btn-warning btn-sm" >Edit</button>
     <button type="submit" class="btn btn-danger btn-sm delete" id="${todo.id}">Delete</button>
   </div>
-`)
+`);
     return $todo;
-  }
+  };
 
   //load Todos by category
   const loadTodos = () => {
@@ -30,9 +30,10 @@ $(function () {
         $(`#tab-${i}`).empty();
         todos.forEach(todo => {
           $(`#tab-${i}`).append(createTodoElement(todo));
-          $(`#${todo.id}`).click(function () {
-            alert(`Alert for ${todo.id}.click() called.`);
-        });
+          $(`#${todo.id}`).click(function() {
+            $(`.${todo.id}`).hide("slide", 1000);
+            $(`#${todo.id}`).text(`${todo.id}-delete`); //
+          });
         });
       });
 
@@ -49,60 +50,66 @@ $(function () {
     });
 
     
-    //auto complete
-    // const databaseAutoComplete = [
-    //   'Love in the Time of Cholera',
-    //   'To Kill a Mockingbird',
-    //   'The Great Gatsby',
-    //   'The Godfather',
-    //   'Lord of the Rings',
-    //   'Forrest Gump',
-    //   'The Matrix',
-    //   'Silk Sleep Mask',
-    //   'Silk Slippers',
-    //   'Silk Robe',
-    //   'Alphabet Soup',
-    // ];
-    // $("#inputText").autocomplete({
-    //   source: databaseAutoComplete
-    // });
 
-  //add count to category tab
-  const categoryCounter = () => {
-    for (let i = 1; i <= 4; i++) {
-      $.get(`api/lists/count/${i}`, (count) => {
-        //update count
-        $(`a[href='#tab-${i}'] .count`).text(`(${count})`);
-      });
-    }
-  };
 
-  // add new todo
-  $('#form-input').on('submit', function (event) {
-    event.preventDefault();
-    let todoName = $('#inputText').val();
-    let category_id = 2;
+    //add count to category tab
+    const categoryCounter = () => {
+      for (let i = 1; i <= 4; i++) {
+        $.get(`api/lists/count/${i}`, (count) => {
+          //update count
+          $(`a[href='#tab-${i}'] .count`).text(`(${count})`);
+        });
+      }
+    };
 
-    $.post('/api/lists', { todo_name: todoName, category_id: category_id })
+    // add new todo
+    $('#form-input').on('submit', function(event) {
+      event.preventDefault();
+      let todoName = $('#inputText').val();
+      let category_id = 2;
 
-      .then((data) => {
-        // add the new todo item to the DOM
-        let $todoItem = createTodoElement(data);
-        $(`#tab-${category_id}`).append($todoItem);
-        $('#inputText').val('');
-        categoryCounter();
-      })
+      $.post('/api/lists', { todo_name: todoName, category_id: category_id })
+
+        .then((data) => {
+          // add the new todo item to the DOM
+          let $todoItem = createTodoElement(data);
+          $(`#tab-${category_id}`).append($todoItem);
+          $('#inputText').val('');
+          categoryCounter();
+        });
+    });
+
+    // delete new todo
+
+  
+  
+    loadTodos();
+    $('.delete').click(function() {
+      alert("Handler for .click() called.");
+    });
+    categoryCounter();
+  
+
   });
 
-  // delete new todo
-
-  
-  
-  loadTodos();
-  $('.delete').click(function () {
-    alert("Handler for .click() called.");
+  //auto complete
+  const databaseAutoComplete = [
+    'Love in the Time of Cholera',
+    'To Kill a Mockingbird',
+    'The Great Gatsby',
+    'The Godfather',
+    'Lord of the Rings',
+    'Forrest Gump',
+    'The Matrix',
+    'Silk Sleep Mask',
+    'Silk Slippers',
+    'Silk Robe',
+    'Alphabet Soup',
+  ];
+  $("#inputText").autocomplete({
+    source: databaseAutoComplete
+  });
 });
-  categoryCounter();
   
 
-});
+//auto complete
--- routes/lists-api.js
+++ routes/lists-api.js
@@ -8,12 +8,12 @@
 const express = require('express');
 const router  = express.Router();
 const userQueries = require('../db/queries/todos');
-const db = require('../connection');
+const db = require('../db/connection.js');
 
 // GET api/lists/:category : show all lists by user
 router.get('/:category', (req, res) => {
   const user_id = req.session.user_id;
-  console.log('cookie',user_id)
+  console.log('cookie',user_id);
   const category_id = req.params.category;
   console.log(category_id);
   userQueries.getActiveTodosByCategory(user_id, category_id)
@@ -30,8 +30,8 @@ router.get('/:category', (req, res) => {
 // GET api/lists/count/:category : show all lists by user
 router.get('/count/:category', (req, res) => {
   const user_id = req.session.user_id;
-  console.log('cookie',user_id)
-  const category_id = req.params.category
+  console.log('cookie',user_id);
+  const category_id = req.params.category;
   console.log(category_id);
   userQueries.getCategoryTodoCount(user_id, category_id)
     .then(lists => {
@@ -52,7 +52,7 @@ router.post('/', (req, res) => {
   userQueries.addTodo(user_id, category_id, todo_name)
     .then(todo => {
       res.json(todo);
-      console.log(todo)
+      console.log(todo);
     })
     .catch(err => {
       res
@@ -80,21 +80,21 @@ router.post('/:id/update', (req, res) => {
 });
 
 // POST api/lists/:id/delete
-router.post('/:id/delete', (req, res) => {
-  const user_id = req.session.user_id;
-  const todo_id = req.body.id;
+// router.post('/:id/delete', (req, res) => {
+//   const user_id = req.session.user_id;
+//   const todo_id = req.body.id;
 
-  userQueries.deleteTodo(user_id, todo_id)
-    .then(todo => {
-      delete db[todo_id];
-      res.json({ todo });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
+//   userQueries.deleteTodo(user_id, todo_id)
+//     .then(todo => {
+//       delete db[todo_id];
+//       res.json({ todo });
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+// });
 
 
 module.exports = router;
